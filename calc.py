baa6f9cb69613957b30372f9362cd4e9b89bd05e
Aditya Dhingra <adhingr@purdue.edu>
2015-10-09 12:04:47 -0400

    Python fixed

--- calc.py
+++ calc.py
@@ -72,8 +72,8 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
-		elif (op == "**"):
-			print "Exponent: ", exp(a, b)
+	elif (op == "**"):
+	    print "Exponent: ", exp(a, b)
         elif (op == "%"):
             print "Modulus: ", mod(a,b)
         else:
